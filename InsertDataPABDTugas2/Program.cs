f49390eaf19e9897482781dc58e199e9b8613cc3
Nopxz <naufalps42@gmail.com>
2022-03-26 01:09:56 +0700

    Add project files.

---
+++ InsertDataPABDTugas2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace InsertDataPABDTugas2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().InsertData();
+        }
+        
+        public void InsertData()
+        {
+
+        }
+    }
+}
