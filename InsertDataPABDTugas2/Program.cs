27d1904d2df10271c0d384772d1a5cafb396626c
Nopxz <naufalps42@gmail.com>
2022-03-26 01:34:26 +0700

    menambahkan insert data pada tabel kasir

--- InsertDataPABDTugas2/Program.cs
+++ InsertDataPABDTugas2/Program.cs
@@ -13,10 +13,34 @@ namespace InsertDataPABDTugas2
         {
             new Program().InsertData();
         }
-        
+
         public void InsertData()
         {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source =NOP\\MSSQLSERVER01;database=Toko_Kue_Naufal;UserID=sa;Password=kontoru555");
+                con.Open();
 
+                SqlCommand cm = new SqlCommand ("insert into Kasir(Id_Kasir, Nama_Kasir, Jenis_Kelamin) values('K000000001', 'Naufal', 'L')"
+                    + "insert into Kasir(Id_Kasir, Nama_Kasir, Jenis_Kelamin) values('K000000002', 'Gita', 'L')"
+                    + "insert into Kasir(Id_Kasir, Nama_Kasir, Jenis_Kelamin) values('K000000003', 'Mahardika', 'L')"
+                    + "insert into Kasir(Id_Kasir, Nama_Kasir, Jenis_Kelamin) values('K000000004', 'Rizky', 'L')"
+                    + "insert into Kasir(Id_Kasir, Nama_Kasir, Jenis_Kelamin) values('K000000005', 'Alam', 'L')", con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Sukses menambahkan data");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Gagal menambahkan data. " + e);
+                Console.ReadKey();
+            }
+            finally
+            {
+                con.Close();
+            }
         }
     }
 }
